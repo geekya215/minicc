6b05d718fad2bcb166d64966c1c6d33be1c90348
Lin Yang <geekya215@gmail.com>
2022-08-16 19:54:06 +0800

    support function call with up to 6 arguments

--- codegen.c
+++ codegen.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 static int depth;
+static char *argreg[] = {"a0", "a1", "a2", "a3", "a4", "a5"};
 
 static void gen_expr(Node *node);
 
@@ -67,9 +68,20 @@ static void gen_expr(Node *node) {
       pop("a1");
       printf("  sd a0, 0(a1)\n");
       return;
-    case ND_FUNCALL:
+    case ND_FUNCALL: {
+      int nargs = 0;
+      for (Node *arg = node->args; arg; arg = arg->next) {
+        gen_expr(arg);
+        push();
+        nargs++;
+      }
+
+      for (int i = nargs - 1; i >= 0; i--)
+        pop(argreg[i]);
+
       printf("  call %s\n", node->funcname);
       return;
+    }
     default:
       break;
   }
