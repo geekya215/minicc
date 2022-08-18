e149aa59e4b4e3ca2ea2351cb9fe693494647dad
Lin Yang <geekya215@gmail.com>
2022-08-18 18:07:23 +0800

    support function definition up to 6 parameters

--- codegen.c
+++ codegen.c
@@ -200,6 +200,11 @@ void codegen(Function *prog) {
     printf("  mv fp, sp\n");
     printf("  addi sp, sp, %d\n", -fn->stack_size);
 
+    // Save passed-by-register arguments to the stack
+    int i = 0;
+    for (Obj *var = fn->params; var; var = var->next)
+      printf("  sd %s, %d(fp)\n", argreg[i++], var->offset);
+
     gen_stmt(fn->body);
     assert(depth == 0);
 
