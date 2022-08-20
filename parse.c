530ed8884d69d69db3dc79346e66b205bd9663fc
Lin Yang <geekya215@gmail.com>
2022-08-20 11:25:25 +0800

    add arrays of arrays

--- parse.c
+++ parse.c
@@ -105,7 +105,7 @@ static Type *func_params(Token **rest, Token *tok, Type *ty) {
 }
 
 // type-suffix = "(" func-params
-//             | "[" num "]"
+//             | "[" num "]" type-suffix
 //             | ε
 static Type *type_suffix(Token **rest, Token *tok, Type *ty) {
   if (equal(tok, "("))
@@ -113,7 +113,8 @@ static Type *type_suffix(Token **rest, Token *tok, Type *ty) {
 
   if (equal(tok, "[")) {
     int sz = get_number(tok->next);
-    *rest = skip(tok->next->next, "]");
+    tok = skip(tok->next->next, "]");
+    ty = type_suffix(rest, tok, ty);
     return array_of(ty, sz);
   }
 
